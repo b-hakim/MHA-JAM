809c50c40b9cf745e0696237772766b4d42fe36d
bassel <bassel.safwat92@gmail.com>
2021-04-16 14:07:48 -0400

    Allowing parameters for each script

--- evaluate_model.py
+++ evaluate_model.py
@@ -2,6 +2,7 @@ import argparse
 import gc
 import math
 import os
+from glob import glob
 
 import scipy
 import tensorflow.keras as k
@@ -18,6 +19,7 @@ def distance_metrics(gt, preds):
         for j in range(errors.shape[1]):
             errors[i, j] = scipy.spatial.distance.euclidean(gt[i, j], preds[i, j])
     return errors.mean(), errors[:, -1].mean(), errors
+
 
 # train the model
 def calculate_ade_fde_gaussian(actual, predicted):
@@ -71,82 +73,82 @@ def calculate_ade_fde_euclidean(actual, predicted):
     return ade, fde
 
 
-
-def run(pdd, mode):
+def run(pdd, mode, model_path, batch_size):
     val_states, val_context, val_map = os.path.join(pdd, "states_val_"+mode+".txt"), \
                                        os.path.join(pdd, "context_val_"+mode+"/"), \
                                        os.path.join(pdd, "maps_val_"+mode+"/")
+    # experimenting one batch
     # val_states, val_context, val_map = os.path.join(pdd, "states_train_"+mode+".txt"), \
     #                                    os.path.join(pdd, "context_train_"+mode+"/"), \
     #                                    os.path.join(pdd, "maps_train_"+mode+"/")
 
     val_states_x, val_states_y, val_context_x = load_data(val_states, val_context)
+    # experimenting one batch
     # val_states_x, val_states_y, val_context_x = val_states_x[:1], val_states_y[:1], val_context_x[:1]
 
     min_ade, min_fde = [100, -1], [100, -1]
-    BATCH_SIZE=8
 
-    if BATCH_SIZE > val_states_y.shape[0]:
+    if batch_size > val_states_y.shape[0]:
         batches = 1
-        BATCH_SIZE = val_states_y.shape[0]
+        batch_size = val_states_y.shape[0]
     else:
-        batches = val_states_y.shape[0]//BATCH_SIZE
+        batches = val_states_y.shape[0]//batch_size
 
-    # for i in range(30, 31):
-        # model = k.models.load_model(f"model_iterations/model_mha_{i}.h5",
-        #                             custom_objects={'euclidean_distance_loss': euclidean_distance_loss,
-        #                                             'gaussian_nll':gaussian_nll})
-    model = k.models.load_model(f"model_iterations/model_mha_best.h5",
-                                custom_objects={'euclidean_distance_loss': euclidean_distance_loss})
-    # model = k.models.load_model(f"model_iterations/model_mha_best.h5",
-    #                             custom_objects={'gaussian_nll':gaussian_nll})
-    predictions = []
+    if os.path.isfile(model_path):
+        count=1
+    else:
+        count = len(glob(model_path+"/*.h5"))
 
-    for b in range(batches):
-        start = b * BATCH_SIZE
-        end = start + BATCH_SIZE
-
-        if b == batches - 1:
-            end = len(val_states_x)
-
-        val_map_x = load_map_batch(val_map, start, end, 'val')
-        # val_map_x = load_map_batch(val_map, start, end, 'train')
-
-        batch_predictions = model.predict([val_states_x[start:end, :, 1:]*100,
-                                           val_context_x[start:end]*100,
-                                           val_map_x], verbose=1)
-
-        if len(predictions) == 0:
-            predictions = batch_predictions
+    for i in range(count):
+        if os.path.isfile(model_path):
+            model = k.models.load_model(f"model_iterations/model_mha_best.h5",
+                                        custom_objects={'euclidean_distance_loss': euclidean_distance_loss,
+                                                        'gaussian_nll':gaussian_nll})
         else:
-            predictions = np.concatenate([predictions, batch_predictions])
-        # break
+            model = k.models.load_model(f"model_iterations/model_mha_{i}.h5",
+                                        custom_objects={'euclidean_distance_loss': euclidean_distance_loss,
+                                                        'gaussian_nll':gaussian_nll})
+        predictions = []
 
-    predictions = np.array(predictions)
+        for b in range(batches):
+            start = b * batch_size
+            end = start + batch_size
 
-    # ade, fde = calculate_ade_fde_gaussian(val_states_y[:, :, 1:3], predictions.reshape(-1, 12, 4))
-    ade, fde = calculate_ade_fde_euclidean(val_states_y[:, :, 1:3], predictions.reshape(-1, 12, 2)/100)
+            if b == batches - 1:
+                end = len(val_states_x)
 
-    # if ade < min_ade[0]:
-    #     min_ade = [ade, i]
-    # if fde < min_fde[0]:
-    #     min_fde = [fde, i]
+            val_map_x = load_map_batch(val_map, start, end, 'val')
+            # val_map_x = load_map_batch(val_map, start, end, 'train') # trying to overfit one/mini sample
 
-    # print("Model", i, ":", ade, fde)
-    # k.backend.clear_session()
-    # gc.collect()
-    # del model
+            batch_predictions = model.predict([val_states_x[start:end, :, 1:]*100,
+                                               val_context_x[start:end]*100,
+                                               val_map_x], verbose=1)
 
-    # print(min_ade, min_fde)
-    print(ade, fde)
-    # print(model.evaluate([val_states_x[start:end,:,1:], val_context_x[start:end]],
-    #                val_states_y[start:end,:,1:3],))
+            if len(predictions) == 0:
+                predictions = batch_predictions
+            else:
+                predictions = np.concatenate([predictions, batch_predictions])
+
+        predictions = np.array(predictions)
+
+        # ade, fde = calculate_ade_fde_gaussian(val_states_y[:, :, 1:3], predictions.reshape(-1, 12, 4))
+        ade, fde = calculate_ade_fde_euclidean(val_states_y[:, :, 1:3], predictions.reshape(-1, 12, 2)/100)
+
+        if ade < min_ade[0]:
+            min_ade = [ade, i]
+        if fde < min_fde[0]:
+            min_fde = [fde, i]
+
+        if not os.path.isfile(model_path):
+            print("Model", i, ":", ade, fde)
+        k.backend.clear_session()
+        gc.collect()
+        del model
+
+    print(min_ade, min_fde)
+
 
 if __name__ == '__main__':
-    # mode = "v1.0-trainval"
-    mode = "v1.0-mini"
-    preprocessed_dataset_dir = "/home/bassel/PycharmProjects/Trajectory-Transformer/datasets/nuscenes/bkup"
-
     parser = argparse.ArgumentParser(description='Calculate the model error rates')
     parser.add_argument('--mode', type=str, default='v1.0-mini')
     parser.add_argument('--preprocessed_dataset_dir', type=str, default='/home/bassel/repos/nuscenes/mha-jam')
@@ -155,4 +157,4 @@ if __name__ == '__main__':
                                     "experiment all models inside the folder")
     args = parser.parse_args()
 
-    run(preprocessed_dataset_dir, mode)
+    run(args.preprocessed_dataset_dir, args.mode, args.model_path)
